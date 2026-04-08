39344c46b2a9bce077dd9d97e9894c7031e9bef1
ensar583403 <120054077+ensar583403@users.noreply.github.com>
2026-04-08 19:14:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const makeCommits = (n) => {
   const x = random.int(0, 54);
   const y = random.int(0, 5);
   const date = moment()
-    .subtract(getRndInteger(0,1), "y")
+    .subtract(getRndInteger(0,5), "y")
     .add(1, "d")
     .add(x, "w")
     .add(y, "d")
